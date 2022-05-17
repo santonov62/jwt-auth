2365aaf73e95d7e77e2e759cea1d48fcc3bade3f
santonov <santonov@nymbus.com>
2022-05-17 13:56:35 +0300

    Axios request interceptor

--- client/my-app/src/http/index.ts
+++ client/my-app/src/http/index.ts
@@ -7,7 +7,7 @@ const api = axios.create({
     baseURL: API_URL
 });
 api.interceptors.request.use(config => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+    config.headers!.Authorization = `Bearer ${localStorage.getItem('token')}`;
     return config;
 });
 
