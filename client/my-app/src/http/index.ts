6a804e6602378f8f6717c5263bb1c693375c7b3b
santonov <santonov@nymbus.com>
2022-05-17 14:20:42 +0300

    Auth service

--- client/my-app/src/http/index.ts
+++ client/my-app/src/http/index.ts
@@ -2,13 +2,13 @@ import axios from "axios";
 
 const API_URL = `http://localhost:5000/api`;
 
-const api = axios.create({
+const $api = axios.create({
     withCredentials: true,
     baseURL: API_URL
 });
-api.interceptors.request.use(config => {
+$api.interceptors.request.use(config => {
     config.headers!.Authorization = `Bearer ${localStorage.getItem('token')}`;
     return config;
 });
 
-export default api;
+export default $api;
