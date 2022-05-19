3af18a9d49eea16a7d3b03e048ba955d6f776d7b
santonov <santonov@nymbus.com>
2022-05-19 18:33:59 +0300

    Response refresh token interceptor

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {AuthResponse} from "../models/response/AuthResponse";
 
 export const API_URL = `http://localhost:5001/api`;
 
@@ -10,5 +11,18 @@ $api.interceptors.request.use(config => {
     config.headers!.Authorization = `Bearer ${localStorage.getItem('token')}`;
     return config;
 });
+
+$api.interceptors.response.use(config => config, async error => {
+    try {
+        const originalRequest = error.config;
+        if (error.response.status === 401) {
+            const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials: true});
+            localStorage.setItem('token', response.data.accessToken);
+            return $api.request(originalRequest);
+        }
+    } catch (e) {
+        console.log('Не авторизован');
+    }
+})
 
 export default $api;
